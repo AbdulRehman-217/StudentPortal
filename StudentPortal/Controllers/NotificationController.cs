02156bc7972310d10db6f28fbdb4834e5b820105
Fiaz <fiazahmad1g@gmail.com>
2020-11-09 10:56:47 +0500

    Add project files.

---
+++ StudentPortal/Controllers/NotificationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult VisitorNotification()
+        {
+            return View();
+        }
+        public IActionResult AllStudentNotification()
+        {
+            return View();
+        }
+        public IActionResult SpecificStudentNotification()
+        {
+            return View();
+        }
+        public IActionResult BatchNotification()
+        {
+            return View();
+        }
+        public IActionResult SectionNotification()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentPortal/Controllers/ResultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.Controllers
+{
+    public class ResultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddNewResult()
+        {
+            return View();
+        }
+    }
+}
